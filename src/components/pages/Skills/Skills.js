4b4c9ceb33d66aee0d1927f414cc7954724f36d8
skim1601 <top030216@gmail.com>
2022-06-15 22:04:34 -0400

    init skills section

--- src/components/pages/Skills/Skills.js
+++ src/components/pages/Skills/Skills.js
@@ -1,7 +1,9 @@
+import "./Skills.css";
+
 function Skills(){
     return (
-        <div>
-            <p> dub </p>
+        <div className = "/skills">
+            <p className = "title"> &#128104;&#8205;&#128187;&nbsp;&nbsp;SKILLS </p>
         </div>
     );
 }
