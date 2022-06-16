7ac881c9a4b8b237e2a346ec5ee926437dcb188a
skim1601 <top030216@gmail.com>
2022-06-16 01:27:45 -0400

    ...

---
+++ src/components/pages/Skills/Languages.js
@@ -0,0 +1,36 @@
+import "./Languages.css";
+import { Html, Css, Js, Python, Java, C } from "../../../assets";
+
+function Languages(){
+    return (
+        <div className = "languages">
+            <p className = "bold"> Languages </p><br/>
+            <div class="tooltip-wrap">
+                <img src={Html} alt='main' /> <br/>
+                <div class="tooltip-content">Html</div> 
+            </div>
+            <div class="tooltip-wrap">
+                <img src={Css} alt='main' /> <br/>
+                <div class="tooltip-content">Css</div> 
+            </div>
+            <div class="tooltip-wrap">
+                <img src={Js} alt='main' /> <br/>
+                <div class="tooltip-content">JavaScript</div> 
+            </div>
+            <div class="tooltip-wrap">
+                <img src={Python} alt='main' /> <br/>
+                <div class="tooltip-content">Python</div> 
+            </div>
+            <div class="tooltip-wrap">
+                <img src={Java} alt='main' /> <br/>
+                <div class="tooltip-content">Java</div> 
+            </div>
+            <div class="tooltip-wrap">
+                <img src={C} alt='main' /> <br/>
+                <div class="tooltip-content">C</div> 
+            </div>
+        </div>
+    );
+}
+
+export default Languages;
