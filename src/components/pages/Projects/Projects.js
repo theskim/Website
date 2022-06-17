9ab2274b0abda324e0ced007ca4c7427f90370cd
skim1601 <top030216@gmail.com>
2022-06-17 18:24:47 -0400

    some stuffs

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,7 +1,32 @@
+import "./Projects.css";
+
 function Projects(){
     return (
         <div>
-            <p> dub </p>
+            <div className = "title-box">
+                <p className = "title">PROJECTS</p>
+            </div>
+            <div className = "projects1">
+                <div className = "project-box">
+                    <div className = "project-title">
+                        <p>Hello Everybody</p>
+                    </div>
+                    <img />
+                    <div className = "project-description">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+            <div className = "projects2">
+                <div className = "project-box">
+                    <div className = "project-title">
+                        <p></p>
+                    </div>
+                    <div className = "project-description">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
