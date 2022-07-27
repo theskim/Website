fd84cf6143b9f1819c0b1f8061e27f3dd0e9a4ce
skim1601 <top030216@gmail.com>
2022-07-27 11:32:17 -0400

    sidebar adjustment 2

--- src/components/NavBar/Sidebar.jsx
+++ src/components/NavBar/Sidebar.jsx
@@ -7,23 +7,23 @@ const Sidebar = () => {
       <Menu>
         <a className="menu-item" href="/" onClick={SmoothScroll}>
             &#127968;&nbsp;&nbsp;Home
-            <div className='right-arrow'>▶</div>
+            <div className='right-arrow'>&#9654;</div>
         </a>
         <a className="menu-item" href="/skills" onClick={SmoothScroll}>
             &#128104;&#8205;&#128187;&nbsp;&nbsp;Skills 
-            <div className='right-arrow'>▶</div>
+            <div className='right-arrow'>&#9654;</div>
         </a>
         <a className="menu-item" href="/experiences" onClick={SmoothScroll}>
             &#128188;&nbsp;&nbsp;Experiences 
-            <div className='right-arrow'>▶</div>
+            <div className='right-arrow'>&#9654;</div>
         </a>
         <a className="menu-item" href="/projects" onClick={SmoothScroll}>
             &#128221;&nbsp;&nbsp;Projects 
-            <div className='right-arrow'>▶</div>
+            <div className='right-arrow'>&#9654;</div>
         </a>
         <a className="menu-item" href="/achievements" onClick={SmoothScroll}>
             &#127942;&nbsp;&nbsp;Achievements 
-            <div className='right-arrow'>▶</div>
+            <div className='right-arrow'>&#9654;</div>
         </a>
       </Menu>
     );
