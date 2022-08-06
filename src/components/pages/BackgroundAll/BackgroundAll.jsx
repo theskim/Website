4f9b7a5b9ab2ef1e5b1443d4dde6cb4cda9867f9
skim1601 <top030216@gmail.com>
2022-08-06 18:15:26 -0400

    netlify issue fixed

--- src/components/pages/BackgroundAll/BackgroundAll.jsx
+++ src/components/pages/BackgroundAll/BackgroundAll.jsx
@@ -1,11 +1,11 @@
-import './Background.css';
+import './Backgroundall.css';
 import { blackBurgerBar } from '../../../assets';
 
 const BackgroundAll = (props) => {
     return (
         <div className="background__all">
             <div className="nav__suggestion">
-                <h4>Press &nbsp;&nbsp;<img src={blackBurgerBar}/>&nbsp;&nbsp;to view other pages</h4>
+                <h4>Press &nbsp;&nbsp;<img src={blackBurgerBar} alt="Nav"/>&nbsp;&nbsp;to view other pages</h4>
             </div>
             <div className="box__all">
                 {props.children}
