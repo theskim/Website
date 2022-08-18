40be4b0d5ebcc38974081c85cfe363970466222b
skim1601 <top030216@gmail.com>
2022-08-18 15:05:43 -0400

    courses table added

---
+++ src/components/pages/Home/DegreeTable.jsx
@@ -0,0 +1,85 @@
+const DegreeTable = () => {
+    const firstYear = {
+        "First Semester": [
+            {"APS100: Orientation to Engineering": "https://engineering.calendar.utoronto.ca/course/aps100h1"}, 
+            {"APS110: Engineering Chemistry & Materials Science": "https://engineering.calendar.utoronto.ca/course/aps105h1"}, 
+            {"APS111: Engineering Strategies & Practice I": "https://engineering.calendar.utoronto.ca/course/aps111h1"},
+            {"CIV100: Mechanics": "https://engineering.calendar.utoronto.ca/course/civ100h1"},
+            {"MAT186: Calculus I": "https://engineering.calendar.utoronto.ca/course/mat186h1"},
+            {"MAT188: Linear Algebra": "https://engineering.calendar.utoronto.ca/course/mat188h1"}
+        ],
+        "Second Semester": [
+            {"APS105: Computer Fundamentals": "https://engineering.calendar.utoronto.ca/course/aps105h1"}, 
+            {"APS112: Engineering Strategies & Practice II": "https://engineering.calendar.utoronto.ca/course/aps112h1"}, 
+            {"ECE110: Electrical Fundamentals": "https://engineering.calendar.utoronto.ca/course/ece110h1"},
+            {"ECE191: Intro to Electrical & Computer Engineering": "https://engineering.calendar.utoronto.ca/course/ece191h1"},
+            {"MAT187: Calculus II": "https://engineering.calendar.utoronto.ca/course/mat187h1"},
+            {"MIE100: Dynamics": "https://engineering.calendar.utoronto.ca/course/mie100h1"}
+        ]
+    };
+    const secondYear = {
+        "First Semester": [
+            {"ECE201: Electrical and Computer Engineering Seminar": "https://engineering.calendar.utoronto.ca/course/ece201h1"}, 
+            {"ECE212: Circuit Analysis": "https://engineering.calendar.utoronto.ca/course/ece212h1"}, 
+            {"ECE241: Digital Systems": "https://engineering.calendar.utoronto.ca/course/ece241h1"},
+            {"ECE244: Programming Fundamentals": "https://engineering.calendar.utoronto.ca/course/ece244h1"},
+            {"MAT290: Advanced Engineering Mathematics": "https://engineering.calendar.utoronto.ca/course/mat290h1"},
+            {"MAT291: Calculus III": "https://engineering.calendar.utoronto.ca/course/mat291h1"}
+        ],
+        "Second Semester": [
+            {"ECE216: Signals and Systems": "https://engineering.calendar.utoronto.ca/course/ece216h1"}, 
+            {"ECE221: Electric and Magnetic Fields": "https://engineering.calendar.utoronto.ca/course/ece221h1"}, 
+            {"ECE231: Introductory Electronics": "https://engineering.calendar.utoronto.ca/course/ece231h1"}, 
+            {"ECE243: Computer Organization": "https://engineering.calendar.utoronto.ca/course/ece243h1"},
+            {"ECE297: Software Communication and Design": "https://engineering.calendar.utoronto.ca/course/ece297h1"},
+            {"": ""},
+        ]
+    };
+
+    const mapObject = (obj) => {
+        return obj["First Semester"].map((x, i) => (
+            <tr>
+                <td>
+                    <a href={Object.values(x)}>{Object.keys(x)}</a>
+                </td>
+                <td>
+                    <a href={Object.values(obj["Second Semester"][i])}>{Object.keys(obj["Second Semester"][i])}</a>
+                </td>
+            </tr>
+        ));
+    }
+
+    return (
+        <details>
+            <summary>My Degree</summary>
+            <span>
+                <details>
+                    <summary>First Year</summary>
+                    <span>
+                        <table>
+                            <tr>
+                                <th>Fall 2021</th>
+                                <th>Winter 2021</th>
+                            </tr>
+                            {mapObject(firstYear)}
+                        </table>
+                    </span>
+                </details>
+                <details>
+                    <summary>Second Year</summary>
+                    <span>
+                        <table>
+                            <tr>
+                                <th>Fall 2022</th>
+                                <th>Winter 2022</th>
+                            </tr>
+                            {mapObject(secondYear)}
+                        </table>
+                    </span>
+                </details>
+            </span>
+        </details>
+    );
+}
+
+export default DegreeTable;
--- src/components/pages/Home/Education.jsx
+++ src/components/pages/Home/Education.jsx
@@ -1,5 +1,6 @@
 import "./Education.css";
 import { UofT } from "../../../assets";
+import DegreeTable from "./DegreeTable";
 
 const Title = () => {
   return ( 
@@ -20,35 +21,7 @@ const Description = () => {
       <h2>
         2021 - 2026 (PEY CO-OP)
       </h2>
-      <select onchange="if(this.value) location.href=(this.value);">
-        <option value="" selected disabled hidden>Courses</option>
-        <optgroup label="Software">
-          <option value="8">APS105: Computer Fundamentals</option>
-        </optgroup>
-        <optgroup label="Electrical">
-          <option value="11">ECE110: Electrical Fundamentals</option>
-        </optgroup>
-        <optgroup label="Engineering Design">
-          <option value="https://engineering.calendar.utoronto.ca/course/aps100h1">APS111: Engineering Strategies & Practice I</option>
-          <option value="7">APS112: Engineering Strategies & Practice II</option>
-        </optgroup>
-        <optgroup label="Mathematics">
-          <option value="6">MAT186: Calculus I</option>
-          <option value="9">MAT187: Calculus II</option>
-          <option value="5">MAT188: Linear Algebra</option>
-        </optgroup>
-        <optgroup label="Physics">
-          <option value="3">CIV100: Mechanics</option>
-          <option value="10">MIE100: Dynamics</option>
-        </optgroup>
-        <optgroup label="Chemistry / Material Science">
-          <option value="4">APS110: Engineering Chemistry & Materials Science</option>
-        </optgroup>
-        <optgroup label="Seminar">
-          <option value="https://engineering.calendar.utoronto.ca/course/aps100h1">APS100: Orientation to Engineering</option>
-          <option value="12">ECE191: Intro to Electrical & Computer Engineering</option>
-        </optgroup>
-      </select>
+      <DegreeTable/>
     </>
   )
 }
