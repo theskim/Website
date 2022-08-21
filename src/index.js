dc1318f0be9fb05c6c0c504496a0be3eaf290846
skim1601 <top030216@gmail.com>
2022-08-20 23:57:13 -0400

    minor filename issue solved

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './Redux/Store';
+import store from './Redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
