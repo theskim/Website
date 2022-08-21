1f51d907f0ac9b6fa0fac9a6511b8a37d156a18e
skim1601 <top030216@gmail.com>
2022-08-21 00:01:50 -0400

    description changE

--- src/components/pages/Experiences/EspDescription.jsx
+++ src/components/pages/Experiences/EspDescription.jsx
@@ -15,14 +15,14 @@ const EspDescription = () => {
             problem solving, systems thinking, and team dynamics. <br/><br/>
 
             As a Project Manager, I was responsible for improving the team's workflow.
-            This was done by implementing Gantt charts and setting internal deadlines to improve team workflow, 
-            resulting in the team completing tasks 1-3 days in advance of actual deadlines<br/><br/>
+            This was done by implementing Gantt charts and setting internal deadlines. 
+            Tis resulted the team to complete tasks 1-3 days in advance of actual deadlines<br/><br/>
 
             The first project was mainly on developing an efficient unloading design for a delivery
             robot. The team's final conceptual design was a hydraulic design.<br/><br/>
             
             The second project was on developing a design for an efficient grime removal on the client's window.
-            As a team, we onducted various experiments at Walberg Chemical Engineering lab 
+            As a team, we conducted various experiments at Walberg Chemical Engineering lab 
             to test the hydrophilicity and transparency of the glass when the TiO2 solution is applied. 
             Ultimately, the team presented a 10-minute long final design to the client and the Engineering Manager
             in a professional manner. <br/><br/>
