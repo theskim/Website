f3b7b78b455b29afaf7769a5af38f8b0d0f04846
skim1601 <top030216@gmail.com>
2022-08-27 22:49:30 -0400

    project setup readjusted: visibility change

--- src/components/pages/Modal/ProjectSetUp.jsx
+++ src/components/pages/Modal/ProjectSetUp.jsx
@@ -1,13 +1,15 @@
 const ProjectSetUp = (props) => {
     return (
         <>
-            { props.setup !== undefined ??
-                <div className="project__setup">
+            { props.setup !== undefined 
+                ? <div className="project__setup">
                     <p>&#9881;&#65039;&nbsp;&nbsp;Set Up:</p>
                     {props.setup.map((command) =>  
                         <code key={command}>{command}</code>
                     )}
-                </div>
+                 </div>
+
+                : <></>
             }
         </>
     );
