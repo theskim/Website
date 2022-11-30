424f966118a75b3b66a9102229a1e35fe252a4da
skim1601 <top030216@gmail.com>
2022-11-30 16:10:39 -0500

    ..

--- src/components/pages/ContactMe/ContactMe.jsx
+++ src/components/pages/ContactMe/ContactMe.jsx
@@ -2,7 +2,7 @@ import ContactMeBox from "./Sections/ContactMeBox";
 import ContactMeTitle from "./Sections/ContactMeTitle";
 import ContactInfo from "./Sections/ContactInfo";
 import SocialMedia from "./Sections/SocialMedia";
-import { ResumeLogo, LinkedInLogo, phoneLogo, mailLogo } from "../../../assets/GeneralLogos";
+import { ResumeLogo, LinkedInLogo, mailLogo } from "../../../assets/GeneralLogos";
 import { GithubLogo } from "../../../assets/ToolLogos";
 import "./ContactMe.scss";
 
@@ -11,9 +11,9 @@ const ContactMe = () => {
       <ContactMeBox>
         <ContactMeTitle/>
         <ContactInfo 
-            src={[phoneLogo, mailLogo]}
-            href={["tel:+1 647-887-1601", "mailto:kimsihy093@gmail.com?subject = Feedback&body = Message"]}
-            display={["647-887-1601", "kimsihy093@gmail.com"]}
+            src={[mailLogo]}
+            href={["mailto:kimsihy093@gmail.com?subject = Feedback&body = Message"]}
+            display={["kimsihy093@gmail.com"]}
         />
         <SocialMedia 
             src={[GithubLogo, LinkedInLogo, ResumeLogo]} 
