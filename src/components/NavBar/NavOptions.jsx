5770aa64b6315911384fd38b4163f5edb4456a5a
skim1601 <top030216@gmail.com>
2023-01-15 13:25:48 -0500

    resume pdf format

--- src/components/NavBar/NavOptions.jsx
+++ src/components/NavBar/NavOptions.jsx
@@ -31,7 +31,7 @@ const NavOptions = (props) => {
         <NavOption to='experiences' title="Experiences"{...props}/>
         <NavOption to='projects' title="Projects"{...props}/>
         <NavOption to='contact' title="Contact"{...props}/>
-        <NavOption href='https://docs.google.com/document/d/10CALRWLR5nVSGOBpfnZh3_F1aLwK4bNRlKlNvgoBtwk/export/pdf' title="Resume" {...props}/>
+        <NavOption href='https://docs.google.com/document/d/10CALRWLR5nVSGOBpfnZh3_F1aLwK4bNRlKlNvgoBtwk/export?format=pdf' title="Resume" {...props}/>
       </>
     );
 }
