02d1b2a79b43ec6f5e16a370206d39b9240fa49e
Sean Kim <40130483+theskim@users.noreply.github.com>
2023-08-12 00:45:13 -0400

    update

--- src/components/pages/Projects/Descriptions/Processor.jsx
+++ src/components/pages/Projects/Descriptions/Processor.jsx
@@ -13,7 +13,7 @@ const Processor = () => {
         >
 I was part of the development of a processor featuring a 16-bit architecture and 8 registers. The processor is capable of executing various instructions like mov, movt, add, sub, ldr, str, and b, among others. <br/><br/>
 
- To debug the SystemVerilog code, I employed an FSM timing diagram from ModelSim. I modified the ALU to implement instructions such as add, sub, and and. Tools like DESim were used to test the processor, which implements a word-matching game. A demonstration of this program is available through the demo link below. Code sharing is restricted due to academic integrity, but it can be provided upon request. <br/><br/>
+ To debug the Verilog code, I employed an FSM timing diagram from ModelSim. I modified the ALU to implement instructions such as add, sub, and and. Tools like DESim were used to test the processor, which implements a word-matching game. A demonstration of this program is available through the demo link below. Code sharing is restricted due to academic integrity, but it can be provided upon request. <br/><br/>
         </Project>
     );
 }
